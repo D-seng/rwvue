ac76d79f7d715370733cf498c2dfa59f2d2a1842
Darren Senger <darren.senger@gmail.com>
2018-12-20 07:14:26 -0800

    daily sync

--- vue-rw/real-world-vue/src/views/EventList.vue
+++ vue-rw/real-world-vue/src/views/EventList.vue
@@ -1,18 +1,32 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    <EventCard/>
-    <BaseIcon/>
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue';
+import EventService from '@/services/EventService.js';
 
 export default {
   // could also use module.exports and require
   components: {
     EventCard
+  },
+  data() {
+    return {
+      events: []
+    };
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data;
+      })
+      .catch(er => {
+        console.log('There was an error: ' + er.response);
+      });
   }
 };
 </script>
